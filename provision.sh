53de38418b9c33bd7a6e2ab0a822530daef0c3ce
Travis N. Thomsen <tthomsen@binaryvoid.com>
2016-05-05 13:29:38 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/puppetlabs.repo
-
-#update and install puppet
 yum update -y
-yum install puppet -y
 yum install git -y
 
-#Copy puppet files
-cp /vagrant/puppet/hiera.yaml /etc/puppet
-cp /vagrant/puppet/Puppetfile /etc/puppet
 cp -r /vagrant/.ssh /root
 chmod 600 /root/.ssh/id_rsa*
-mkdir /etc/puppet/manifests
-cp /vagrant/puppet/site.pp /etc/puppet/manifests
-cp /vagrant/puppet/common.json /var/lib/hiera
 
 #add ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
